b90136ec8c703f66012c011fedddb12717f1e7bc
Peter Lombardo <peterllombardo@gmail.com>
2023-08-21 05:10:35 -0400

    Updated functionality with async/await

--- Deck of Cards/deckOfCards.js
+++ Deck of Cards/deckOfCards.js
@@ -13,8 +13,46 @@ function displayCard(URL){
 }
 
 
-function pickACard(){
-        axios.get(`${pickUrl}/${deck_id}/draw/?count=1`)
+// function pickACard(){
+//         axios.get(`${pickUrl}/${deck_id}/draw/?count=1`)
+//             .then(response => {
+//                 if (response.data.cards.length > 0) {
+//                     let value = response.data.cards[0].value;
+//                     let suit = response.data.cards[0].suit;
+//                     let URL = response.data.cards[0].image;
+//                     console.log(`${value} of ${suit}`);
+//                     displayCard(URL);
+//                 } else {
+//                     console.log("No more cards in the deck.");
+//                     cardButton.disabled = true;
+//                 }
+//             })
+//             .catch(err => console.log(err));
+//     }
+
+// function initializeDeck() {
+//     axios.get(`${deckUrl}`)
+//         .then(response => {
+//         console.log("Deck initialized.");
+//         deck_id = response.data.deck_id;
+//         cardButton.disabled = false;})
+//             .catch(err => console.log(err));
+//     }   
+
+
+
+async function initializeDeck() {
+    await axios.get(`${deckUrl}`)
+        .then(response => {
+        console.log("Deck initialized.");
+        deck_id = response.data.deck_id;
+        cardButton.disabled = false;})
+            .catch(err => console.log(err));
+    }   
+
+
+async function pickACard(){
+       await axios.get(`${pickUrl}/${deck_id}/draw/?count=1`)
             .then(response => {
                 if (response.data.cards.length > 0) {
                     let value = response.data.cards[0].value;
@@ -30,14 +68,5 @@ function pickACard(){
             .catch(err => console.log(err));
     }
 
-function initializeDeck() {
-    axios.get(`${deckUrl}`)
-        .then(response => {
-        console.log("Deck initialized.");
-        deck_id = response.data.deck_id;
-        cardButton.disabled = false;})
-            .catch(err => console.log(err));
-    }   
-
-window.onload = initializeDeck;
-cardButton.addEventListener("click", pickACard);
+    window.onload = initializeDeck;
+    cardButton.addEventListener("click", pickACard);
--- NumberFacts/asyncProm.js
+++ NumberFacts/asyncProm.js
@@ -8,20 +8,66 @@ function displayFactList(listId, fact) {
     ul.appendChild(li);
 }
 
-function getRandomFacts(){
-for (let i = 1; i < 5; i++){
-    let randInt = Math.floor(Math.random() * 100) + 1;
-    axios.get(`${baseUrl}${randInt}`)
-    .then(response => displayFactList('randomFactsList', response.data))
-    .catch(err => console.log(err))};
-};
+// function getRandomFacts(){
+// for (let i = 1; i < 5; i++){
+//     let randInt = Math.floor(Math.random() * 100) + 1;
+//     axios.get(`${baseUrl}${randInt}`)
+//     .then(response => displayFactList('randomFactsList', response.data))
+//     .catch(err => console.log(err))};
+// };
 
-function getFavoriteFacts(){
-    for (let i = 1; i < 5; i++){
-        axios.get(`${baseUrl}3`)
-        .then(response => displayFactList('favoriteFactsList', response.data))
+// function getFavoriteFacts(){
+//     for (let i = 1; i < 5; i++){
+//         axios.get(`${baseUrl}3`)
+//         .then(response => displayFactList('favoriteFactsList', response.data))
+//         .catch(err => console.log(err));
+//     };
+// };
+// getRandomFacts()
+// getFavoriteFacts()
+
+async function getRandomFacts() {
+    const requests = [];
+
+    for (let i = 1; i < 5; i++) {
+        let randInt = Math.floor(Math.random() * 100) + 1;
+        const request = axios.get(`${baseUrl}${randInt}`)
+            .then(response => response.data)
+            .catch(err => console.log(err));
+
+        requests.push(request);
+    }
+
+    await Promise.all(requests)
+        .then(factDataArray => {
+            for (const factData of factDataArray) {
+                displayFactList('randomFactsList', factData);
+            }
+        })
         .catch(err => console.log(err));
-    };
-};
-getRandomFacts()
-getFavoriteFacts()
+}
+
+async  function getFavoriteFacts() {
+    const requests = [];
+
+    for (let i = 1; i < 5; i++) {
+        let favInt = 3;
+        const request = axios.get(`${baseUrl}${favInt}`)
+            .then(response => response.data)
+            .catch(err => console.log(err));
+
+        requests.push(request);
+    }
+
+    await Promise.all(requests)
+        .then(factDataArray => {
+            for (const factData of factDataArray) {
+                displayFactList('favoriteFactsList', factData);
+            }
+        })
+        .catch(err => console.log(err));
+}
+
+getFavoriteFacts();
+
+getRandomFacts();
